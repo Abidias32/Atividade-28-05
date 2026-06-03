09d84c681b0e79e586540679ca0b5f412b07c157
Abidias32 <abidiasleite32@gmail.com>
2026-06-02 21:41:49 -0300

    atividade pratica

---
+++ index.js
@@ -0,0 +1,27 @@
+//calcular o dobro de um número
+//let numero = 10;
+//let dobro = numero * 2;
+//console.log("Número:", numero);
+//console.log("Dobro:", dobro);
+
+//-------------------------------------------------------------------------
+
+//trocar o valor de duas variáveis sem usar uma terceira variável
+
+//let a = 5;
+//let b = 10;
+//let caixa;
+//caixa = a;
+//a = b;
+//b = caixa;
+//console.log("Valor de a:", a);
+//console.log("Valor de b:", b); 
+
+//--------------------------------------------------------------------------
+
+//calcular a média de três notas
+let nota1 = 7;
+let nota2 = 8;
+let nota3 = 9;
+let media = (nota1 + nota2 + nota3) / 3;
+console.log("Média:", media);
